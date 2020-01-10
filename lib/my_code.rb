16dd8e053ceb0f38963cbbe40b1c7cac018b1d63
Alehandro Persaud <andrew041994@gmail.com>
2020-01-10 05:21:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,3 @@
-# My Code here....
 def map_to_negativize(source_array)
   new_array=[]
    i=0
@@ -56,12 +55,3 @@ def reduce_to_all_true(source_array)
     true
 end
 
-def reduce_to_all_true(source_array)
-  i=0
-  while i < source_array.size
-    if source_array[i]
-    end
-    i+=1
-  end
-
-end
